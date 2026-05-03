c36e4baaf0aac9d64dad69fbbed87f44bb4de10c
Amir3356 <amirsiraj1995@gmail.com>
2026-05-03 22:18:41 +0300

    tttt

--- backend/database/migrations/2024_01_01_000000_create_users_table.php
+++ backend/database/migrations/2024_01_01_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->enum('role', ['admin', 'employer', 'job_seeker'])->default('job_seeker');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
         });
