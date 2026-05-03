32f37c171aea2752c28a8790be649ee83da3e3f0
Amir3356 <amirsiraj1995@gmail.com>
2026-05-03 22:22:15 +0300

    oiiiiiii

--- backend/app/Models/Application.php
+++ backend/app/Models/Application.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
 {
-    const UPDATED_AT = null;
-
     protected $fillable = [
         'user_id',
         'job_id',
--- backend/app/Models/Job.php
+++ backend/app/Models/Job.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-    const UPDATED_AT = null;
-
     protected $fillable = [
         'user_id',
         'title',
